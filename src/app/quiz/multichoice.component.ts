e816ca97210cf87ff2d966b8f8c200cdc0c6de53
Rucha Mahajan <rucha.mahajan@gmail.com>
2018-08-30 18:37:50 -0400

    added folders

---
+++ src/app/quiz/multichoice.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './multichoice.component.html',
+})
+
+export class MultichoiceComponent {
+  title = 'Krishna Consciousness Quiz';
+}
