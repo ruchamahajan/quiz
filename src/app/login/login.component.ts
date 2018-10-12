5073904b0ac441fec4256e9e582b1c635dac3349
Rucha Mahajan <rucha.mahajan@gmail.com>
2018-10-11 22:24:10 -0400

    login page changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  private viewRegister = false;
+  private viewLogin = true;
+
+  private loginForm = new FormGroup ({
+    userName: new FormControl(''),
+    passWord: new FormControl('')
+  });
+
+   private registerForm = new FormGroup({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    dateOfBirth: new FormControl(),
+    gender: new FormControl(),
+    email: new FormControl(),
+    registerusername: new FormControl(),
+    registerpassword: new FormControl(),
+    securtyQuestion: new FormControl(),
+    answer: new FormControl()
+  });
+
+  constructor(fb: FormBuilder) { }
 
   ngOnInit() {
   }
+
+
+  viewLoginForm() {
+    this.viewLogin = true;
+    this.viewRegister = false;
+
+  }
+
+  viewRegisterForm() {
+    this.viewLogin = false;
+    this.viewRegister = true;
+
+   }
 
 }
