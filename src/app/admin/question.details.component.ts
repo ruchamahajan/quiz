557497f26659452a2379e3f82e34f2dadefcdf95
Rucha Mahajan <rucha.mahajan@gmail.com>
2018-10-14 18:48:44 -0400

    minor changes

---
+++ src/app/admin/question.details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './question.details.component.html',
+  })
+
+export class QuestionDetailsComponent {
+    title = 'Krishna Consciousness Quiz';
+}
